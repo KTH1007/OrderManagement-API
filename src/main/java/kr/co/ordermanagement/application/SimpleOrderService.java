2581500a38b0886d8a20838c0053d334e8580190
KTH1007 <rlaxogjs2684@gmail.com>
2024-07-18 15:08:06 +0900

    Feat: 주문 취소 API Closes #3

--- src/main/java/kr/co/ordermanagement/application/SimpleOrderService.java
+++ src/main/java/kr/co/ordermanagement/application/SimpleOrderService.java
@@ -59,6 +59,13 @@ public class SimpleOrderService {
         return orderResponseDtos;
     }
 
+    public OrderResponseDto cancelOrderById(Long orderId) {
+        Order order = orderRepository.findById(orderId);
+        order.cancel();
+        OrderResponseDto orderResponseDto = OrderResponseDto.toDto(order);
+        return orderResponseDto;
+    }
+
     private List<OrderedProduct> makeOrderedProducts(List<OrderProductRequestDto> orderProductRequestDtos) {
         return orderProductRequestDtos
                 .stream()
